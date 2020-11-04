967f2b20d9b2a1717f8ec4b37f6362f01a6fc6f1
Allan Benedito <allan.benedito@gmail.com>
2020-11-03 22:46:37 -0300

    Aula 34

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import AdPage from './pages/AdPage';
 import AddAd from './pages/AddAd';
+import Ads from './pages/Ads';
 
 
 function Routes() {
@@ -32,6 +33,9 @@ function Routes() {
             </RouteHandler>
             <RouteHandler private exact path="/post-an-ad">
                 <AddAd   />
+            </RouteHandler>
+            <RouteHandler exact path="/ads">
+                <Ads   />
             </RouteHandler>
             <RouteHandler>
                 <NotFound />
--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import { PageArea, SearchArea } from './styled';
+import { PageArea } from './styled';
 import useApi from '../../helpers/OlxAPI';
 
 import { PageContainer } from '../../components/MainComponents';
@@ -42,47 +42,37 @@ const Page = () => {
     }, []);
 
     return (
-        <>
-            <SearchArea>
-                <PageContainer>
-                    <div className="searchBox">
-                        <form method="GET" action="/ads">
-                            <input type="text" name="q" placeholder="O que você Procura" />
-                            <select name="state">
+        <PageContainer>
+            <PageArea>
+                <div className="leftSide">
+                    <form method="GET">
+                        <input type="text" name="q" />
 
-                                {statelist.map((i, key) =>
-                                    <option key={key} value={i.name}>{i.name}</option>
-                                )}
-                            </select>
-                            <button>Pesquisar</button>
-                        </form>
-                    </div>
-                    <div className="categoryList">
-                        {categories.map((i, key) =>
-                            <Link key={key} to={`ads?cat=${i.slug}`} className="categoryItem">
-                                <img src={i.img} alt="" />
-                                <span>{i.name}</span>
-                            </Link>
-                        )}
-                    </div>
-                </PageContainer>
-            </SearchArea>
-            <PageContainer>
-                <PageArea>
-                    <h2>Anúncios Recentes</h2>
-                    <div className="list">
-                        {adList.map((i,key)=>
-                            <AdItem key={key} data={i} />
-                        )}
-                    </div>
-                    <Link to="/ads" clasName="seeAllLink">Ver todos</Link>
-                    <hr />
+                        <div className="filterName">Estado:</div>
+                        <select name="state">
+                            <option></option>
+                            {statelist.map((i,key)=>
+                                <option key={key} value={i.name}>{i.name}</option>
+                            )}
+                        </select>
 
-                    texto da pagina
-                </PageArea>
-            </PageContainer>
-        </>
-    )
+                        <div className="filterName">Categoria:</div>
+                        <ul>
+                            {categories.map((i,key)=>
+                                <li key={key} className="categoryItem">
+                                    <img src={i.img} alt="" />
+                                    <span>{i.name}</span>
+                                </li>
+                            )}
+                        </ul>
+                    </form>
+                </div>
+                <div className="rightSide">
+                    ...
+                </div>
+            </PageArea>
+        </PageContainer>
+    );
 }
 
 export default Page;
