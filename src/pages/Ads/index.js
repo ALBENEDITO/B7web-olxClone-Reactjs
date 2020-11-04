908de91f628d620a287b6f33f3af4d72e2328a92
Allan Benedito <allan.benedito@gmail.com>
2020-11-03 23:57:41 -0300

    Aula 34

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import { PageArea } from './styled';
 import useApi from '../../helpers/OlxAPI';
 
@@ -9,6 +9,16 @@ import AdItem from '../../components/partials/AdItem';
 const Page = () => {
 
     const api = useApi();
+
+    const useQueryString = () => {
+        return new URLSearchParams (useLocation().search );
+    }
+    const query = useQueryString();
+
+    const [q, setQ] = useState(query.get('q') != null ? query.get('q') : '' );
+    const [cat, setCat] = useState(query.get('cat') != null ? query.get('cat') : '' );
+    const [state, setState] = useState(query.get('state') != null ? query.get('state') : '' );
+
 
     const [statelist, setStateList] = useState([]);
     const [categories, setCategories] = useState([]);
@@ -46,10 +56,10 @@ const Page = () => {
             <PageArea>
                 <div className="leftSide">
                     <form method="GET">
-                        <input type="text" name="q" />
+                        <input type="text" name="q" value={q} placeholder="Pesquisar" />
 
                         <div className="filterName">Estado:</div>
-                        <select name="state">
+                        <select name="state" value={state}>
                             <option></option>
                             {statelist.map((i,key)=>
                                 <option key={key} value={i.name}>{i.name}</option>
@@ -59,7 +69,7 @@ const Page = () => {
                         <div className="filterName">Categoria:</div>
                         <ul>
                             {categories.map((i,key)=>
-                                <li key={key} className="categoryItem">
+                                <li key={key} className={cat==i.slug?'categoryItem active':'categoryItem'}>
                                     <img src={i.img} alt="" />
                                     <span>{i.name}</span>
                                 </li>
